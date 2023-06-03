f8ef57022ea6f236360a83c873b9b20910d1ed37
Vladimir Dronov <ffonord@gmail.com>
2023-06-03 09:51:39 +0300

    Add video export script

---
+++ video_export.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+cameraIp=192.168.1.133
+ftpWorkTime=3600
+
+cameraNotExists=$(ping $cameraIp -c 1 | grep '0 received')
+
+[ "$cameraNotExists" ] && echo 'Camera not found' && exit
+
+echo "Start ftp server "
+setsid $( (
+  sleep 1
+  echo root
+  sleep 1
+  echo '/tmp/fuse_d/ftp.sh'
+  sleep $ftpWorkTime
+  echo exit
+  sleep 1
+) | telnet $cameraIp) >/dev/null 2>&1 </dev/null &
+
+i=1
+sp="/-\|"
+
+while [ -z $(ps -fade | grep "sleep $ftpWorkTime" | grep -v grep | awk '{print $2}') ]; do
+  printf "\b${sp:i++%${#sp}:1}"
+done
+sleep 1
+
+echo "Start download videos, ip: $cameraIp"
+dirPath=$(date +%F)
+[ ! -d "$dirPath" ] && mkdir $dirPath
+
+ftpPath=$cameraIp/DCIM/101MEDIA/
+
+#fetch files list
+wget --no-remove-listing --user=root ftp://$ftpPath >/dev/null 2>&1
+#only filenames *.MP4
+fileNames="$(cat .listing | sed -n '/.MP4/p' | awk '{print $NF}')"
+
+for fileName in $fileNames; do
+  mp4File="${fileName%.*}".MP4
+  secFile="${fileName%.*}".SEC
+
+  filePath=$dirPath/$mp4File
+
+  wget --no-remove-listing \
+    --no-parent --recursive \
+    --level=1 --no-directories \
+    --no-host-directories \
+    --user=root \
+    --no-clobber \
+    --continue \
+    -O $filePath \
+    ftp://$ftpPath$mp4File
+
+  localFileSize=$(stat -c%s "$filePath")
+  externalFileSize=$(cat .listing | grep "$mp4File" | cut -d " " -f 22)
+
+  if [ "$localFileSize" = "$externalFileSize" ]; then
+    curl ftp://$ftpPath -X "DELE $mp4File" --user root:
+    curl ftp://$ftpPath -X "DELE $secFile" --user root:
+  else
+    echo "Files not equals $mp4File, local: $localFileSize, external: $externalFileSize"
+    rm $filePath
+  fi
+done
+
+echo "Stop ftp server"
+kill -9 $(ps -fade | grep "sleep $ftpWorkTime" | grep -v grep | awk '{print $2}')
+rm ".listing" index.html*
